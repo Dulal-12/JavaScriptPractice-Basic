85a329d6f5c56cb7aa1c974ae9c66b0d82d27368
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-03 15:32:59 +0600

    Done recursive way to solve factorial

--- factorialRecursive.js
+++ factorialRecursive.js
@@ -1,3 +1,11 @@
-const factorial = (num)=>{
-    if(num)
+function fact(num){
+    if (num === 0 || num ===1){
+        return 1;
+    }
+    else{
+        return num*fact(num-1);
+    }
 }
+
+const result = fact(10);
+console.log(result);                            //4*3*2*1    4*3*2*1
